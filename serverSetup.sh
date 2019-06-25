84911a68c0f2811da2098b74a5367fd302e2b0aa
HMS <harrisonsbennett@gmail.com>
2019-06-25 13:44:39 +0100

    Version Two

--- serverSetup.sh
+++ serverSetup.sh
@@ -2,7 +2,11 @@ az group create --resource-group JenkinsGroup
 
 az network vnet create --resource-group JenkinsGroup --name JenkinsNetwork --address-prefixes 10.0.0.0/16 --subnet-name JenkinsSubnet --subnet-prefix 10.0.0.0/24
 
-az network vnet subnet create --resource-group JenkinsGroup --vnet-name JenkinsNetwork --name JenkinsSubnet --address-prefixes 10.0.0.0/24
+az network vnet subnet create --resource-group JenkinsGroup --vnet-name JenkinsNetwork --name JenkinsSubnetHost --address-prefixes 10.0.10.21/24
+
+az network vnet subnet create --resource-group JenkinsGroup --vnet-name JenkinsNetwork --name JenkinsSubnetSlave --address-prefixes 10.0.10.31/24
+
+az network vnet subnet create --resource-group JenkinsGroup --vnet-name JenkinsNetwork --name JenkinsSubnetPython --address-prefizes 10.0.10.41/24
 
 az network nsg create --resource-group JenkinsGroup --name JenkinsNSG
 
@@ -12,10 +16,14 @@ az network nsg rule create --resource-group JenkinsGroup --name SSH --destinatio
 
 az network public-ip create --resource-group JenkinsGroup --name JenkinsIP --dns-name bazzatron3000 --allocation-method Static
 
-az network nic create --resource-group JenkinsGroup --name JenkinsNIC --vnet-name JenkinsNetwork --subnet JenkinsSubnet --network-security-group JenkinsNSG
+az network nic create --resource-group JenkinsGroup --name JenkinsNICHost --vnet-name JenkinsNetwork --subnet JenkinsSubnetHost --network-security-group JenkinsNSG
 
-az vm create --resource-group JenkinsGroup --name JenkinsHostVM --image CentOS --nics JenkinsNIC
+az network nic create --resource-group JenkinsGroup --name JenkinsNICSlave --vnet-name JenkinsNetwork --subnet JenkinsSubnetSlave --network-security-group JenkinsNSG
 
-az vm create --resource-group JenkinsGroup --name JenkinsSlaveVM --image CentOS --nics JenkinsNIC
+az network nic create --resource-group JenkinsGroup --name JenkinsNICPython --vnet-name JenkinsNetwork --subnet JenkinsSubnetPython --network-security-group JenkinsNSG
 
-az vm create --resource-group JenkinsGroup --name PythonServerVM --image CentOS --nics JenkinsNIC
+az vm create --resource-group JenkinsGroup --name JenkinsHostVM --image UbuntuLTS --nics JenkinsNIC
+
+az vm create --resource-group JenkinsGroup --name JenkinsSlaveVM --image UbuntuLTS --nics JenkinsNIC
+
+az vm create --resource-group JenkinsGroup --name PythonServerVM --image UbuntuLTS --nics JenkinsNIC
